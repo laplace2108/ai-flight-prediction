05dd0c44a54afb2ccfb8e4ba34e5bad2872e8198
Yosdel Plasencia Montesino <yplasencia@MacBook-Pro-de-Yosdel.local>
2021-06-07 19:08:00 -0500

    Add the project

---
+++ services/src/main/java/com/middleware/services/dbconnection/nationalweatherservices/station/observation/models/Elevation.java
@@ -0,0 +1,52 @@
+
+package com.middleware.services.dbconnection.nationalweatherservices.station.observation.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Elevation {
+
+    @SerializedName("value")
+    @Expose
+    private long value;
+    @SerializedName("unitCode")
+    @Expose
+    private String unitCode;
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    public String getUnitCode() {
+        return unitCode;
+    }
+
+    public void setUnitCode(String unitCode) {
+        this.unitCode = unitCode;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(Elevation.class.getName()).append('@').append(Integer.toHexString(System.identityHashCode(this))).append('[');
+        sb.append("value");
+        sb.append('=');
+        sb.append(this.value);
+        sb.append(',');
+        sb.append("unitCode");
+        sb.append('=');
+        sb.append(((this.unitCode == null)?"<null>":this.unitCode));
+        sb.append(',');
+        if (sb.charAt((sb.length()- 1)) == ',') {
+            sb.setCharAt((sb.length()- 1), ']');
+        } else {
+            sb.append(']');
+        }
+        return sb.toString();
+    }
+
+}
